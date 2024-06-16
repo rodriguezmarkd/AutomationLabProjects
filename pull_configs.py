fadce381ea44c272b40a38e20309fd7cba8ca814
Mark Rodriguez <rodriguezmarkd@gmail.com>
2024-06-16 14:48:46 -0500

    Changed Logic - added more device support

--- pull_configs.py
+++ pull_configs.py
@@ -1,19 +1,23 @@
 from netmiko import Netmiko
 
-my_device = {
-	'host': '10.32.2.1',
-	'username': 'mark',
-	'password': 'mark',
-	'device_type': 'cisco_ios',
-}
+hosts = ['10.32.1.210', '10.32.1.211', '10.32.1.212', '10.32.1.213']
 
-net_conn = Netmiko(**my_device)
-output = net_conn.send_command_timing('copy running-config tftp://10.32.1.90/MarkslanR_config.txt')
-if 'Address' in output:
-	output += net_conn.send_command_timing("\n")
 
-if 'Destination' in output:
-	output += net_conn.send_command_timing("\n")
+for host in hosts:
+	my_device = {
+		'host': host,
+		'username': 'mark',
+		'password': 'mark',
+		'device_type': 'cisco_ios',
+	}
+
+	net_conn = Netmiko(**my_device)
+	output = net_conn.send_command_timing(f'copy running-config tftp://10.32.1.90/{host}.txt')
+	if 'Address' in output:
+		output += net_conn.send_command_timing("\n")
+
+	if 'Destination' in output:
+		output += net_conn.send_command_timing("\n")
 
 net_conn.disconnect()
 
