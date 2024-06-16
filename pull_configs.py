bf0b93d6a03b1e78e4d6b829fea34284c87bff11
Mark Rodriguez <rodriguezmarkd@gmail.com>
2024-06-15 22:26:01 -0500

    Fixed config downloader

--- pull_configs.py
+++ pull_configs.py
@@ -1,7 +1,7 @@
-from Netmiko import Netmiko
+from netmiko import Netmiko
 
 my_device = {
-	'host': '10.32.1.151',
+	'host': '10.32.2.1',
 	'username': 'mark',
 	'password': 'mark',
 	'device_type': 'cisco_ios',
@@ -10,11 +10,11 @@ my_device = {
 net_conn = Netmiko(**my_device)
 output = net_conn.send_command_timing('copy running-config tftp://10.32.1.90/MarkslanR_config.txt')
 if 'Address' in output:
-	output += net_connect.send_command_timing("\n")
+	output += net_conn.send_command_timing("\n")
 
 if 'Destination' in output:
-	output += net_connect.send_command_timing("\n")
+	output += net_conn.send_command_timing("\n")
 
-net_connect.disconnect()
+net_conn.disconnect()
 
 print(output)
